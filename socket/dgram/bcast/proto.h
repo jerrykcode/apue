fc46e703449b00c9e1fb08a6aacf4312b6465c14
jerrykcode <jerrykang026@gmail.com>
2021-03-03 22:19:10 +0800

    多播实例

---
+++ socket/dgram/bcast/proto.h
@@ -0,0 +1,17 @@
+#ifndef PROTO_H__
+#define PROTO_H__
+
+#define RCVPORT "2001"
+
+#define NAMESIZE 25
+#define NUM_SUBJECTS 4
+
+struct msg_st {
+    uint8_t name[NAMESIZE];
+    uint16_t score[NUM_SUBJECTS];
+}__attribute__((packed));
+
+
+
+
+#endif
